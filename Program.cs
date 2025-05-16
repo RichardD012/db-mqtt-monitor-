678e77ce75538f49d322b8455ee65a722d3f5d4c
Richard Dyer <Richard@r7development.com>
2025-05-16 17:48:41 -0600

    Initial checkin

---
+++ Program.cs
@@ -0,0 +1,72 @@
+﻿using System.Text.Json;
+using MQTTnet;
+using MQTTnet.Protocol;
+
+// See https://aka.ms/new-console-template for more information
+DotNetEnv.Env.Load(".env");
+
+string GetRequiredEnvVar(string name)
+{
+    var value = Environment.GetEnvironmentVariable(name);
+    if (string.IsNullOrEmpty(value))
+    {
+        Console.WriteLine($"Error: Required environment variable '{name}' is not set");
+        Environment.Exit(1);
+    }
+    return value;
+}
+
+// Get the database size
+long GetDbSize(string path)
+{
+    try
+    {
+        FileInfo fileInfo = new FileInfo(path);
+        return fileInfo.Length;
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Error: {ex.Message}");
+        return 0;
+    }
+}
+
+var dbPath = GetRequiredEnvVar("DB_PATH");
+var mqttBroker = GetRequiredEnvVar("MQTT_BROKER");
+var mqttTopic = GetRequiredEnvVar("MQTT_TOPIC");
+var mqttUsername = GetRequiredEnvVar("MQTT_USERNAME");
+var mqttPassword = GetRequiredEnvVar("MQTT_PASSWORD");
+
+
+var mqttFactory = new MqttClientFactory();
+
+using (var mqttClient = mqttFactory.CreateMqttClient())
+{
+    var mqttClientOptions = new MqttClientOptionsBuilder()
+        .WithTcpServer(mqttBroker)
+        .WithCredentials(mqttUsername, mqttPassword)
+        .WithClientId($"plex-db-monitor")
+        .WithCleanSession()
+        .WithKeepAlivePeriod(TimeSpan.FromSeconds(60))
+        .WithTimeout(TimeSpan.FromSeconds(10))
+        .Build();
+
+    await mqttClient.ConnectAsync(mqttClientOptions, CancellationToken.None);
+    
+    var deviceConfig = new
+    {
+        timestamp = DateTime.UtcNow,
+        db_size_bytes = GetDbSize(dbPath),
+    };
+    
+    await mqttClient.PublishAsync(new MqttApplicationMessageBuilder()
+        .WithTopic(mqttTopic)
+        .WithPayload(JsonSerializer.Serialize(deviceConfig))
+        .WithQualityOfServiceLevel(MqttQualityOfServiceLevel.AtLeastOnce)
+        .WithRetainFlag()
+        .Build());
+
+    await mqttClient.DisconnectAsync();
+
+    Console.WriteLine("MQTT application message is published.");
+}
